545a1c029493947c9c72f4c0d5248e573678511e
Kelsie Merchant <eislek02@gmail.com>
2020-09-23 10:50:42 -0500

    Add Python recursive API call to get count of keywords in titles from hot articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,7 +5,7 @@ to parse title of all hot article and print sorted count
 """
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, after=None, count={}):
     """
     queries the Reddit API
     parses title of all hot articles
@@ -21,13 +21,39 @@ def count_words(subreddit, word_list):
     subreddit_info = requests.get(sub_URL,
                                   headers={"user-agent": "user"},
                                   allow_redirects=False).json()
-    if "data" not in subreddit_info and hot_list == []:
+    for word in word_list:
+        word = word.lower()
+        if word not in count.keys():
+            count[word] = 0
+    if "data" not in subreddit_info:
         return None
     children = subreddit_info.get("data").get("children")
     for child in children:
-        hot_list.append(child.get("data").get("title"))
-        count += 1
+        title = (child.get("data").get("title"))
+        title = title.split(' ')
+        for word in title:
+            word = word.lower()
+            if word in word_list:
+                count[word] += 1;
     after = subreddit_info.get("data").get("after")
     if after is None:
-        return hot_list
-    return (recurse(subreddit, hot_list, after, count))
+        result = []
+        for k in count.keys():
+            if result == []:
+                result.append("{}: {}".format(k, count[k]))
+            else:
+                for i in range(len(result)):
+                    if count[k] > int(result[i].split(' ')[1]):
+                        result = result[:i] + \
+                                 ["{}: {}".format(k, count[k])] + \
+                                 result[i:]
+                        break;
+                    else:
+                        continue
+                else:
+                    result.append("{}: {}".format(k, count[k]))
+        for printing in result:
+            if int(printing.split(' ')[1]) != 0:
+                print(printing)
+        return
+    return (count_words(subreddit, word_list, after, count))
